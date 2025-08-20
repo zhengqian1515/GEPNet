43c1a4f82109e3c44fa980e678946013167bf3bf
zhengqian1515 <131568358+zhengqian1515@users.noreply.github.com>
2025-08-20 09:45:24 +0800

    Add files via upload
    8.20update

---
+++ GepNet_PET/evaluation_scripts/run_evaluation.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+export PYTHONPATH=/data/MedSegmentation/ZXY/GEPNet-main/GepNet_PET/DATASET
+
+REF_FOLDER="$PYTHONPATH"/DATASET/gepnet_trained_models/gepnet/3d_fullres/Task503_ecpc/gepnet_trainer_PET__nnFormerPlansv2.1_trgSp_1x1x1/gt_niftis   # ground truth
+PRED_FOLDER="$PYTHONPATH"/DATASET/gepnet_trained_models/gepnet/3d_fullres/Task503_ecpc/gepnet_trainer_PET__nnFormerPlansv2.1_trgSp_1x1x1/pred_niftis  # pred
+
+
+LABELS="0 1 2"
+python ../gepnet/evaluation/evaluator.py -ref $REF_FOLDER -pred $PRED_FOLDER -l $LABELS
